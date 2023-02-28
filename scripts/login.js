a81c82123e61b1c29984f7e629c5ae049a82ece0
Luis Alfonso Martinez Martinez <luisalfonsomartinezmartinez@outlook.es>
2023-02-27 22:40:51 -0600

    CAmbio de algunas cosas

---
+++ scripts/login.js
@@ -0,0 +1,13 @@
+class user{
+    constructor(user,password){
+        this.password=password;
+        this.user=user;
+    }
+}
+
+let formLogin =document.getElementById("form-login");
+
+formLogin.addEventListener("submit", (e)=>{
+   e.preventDefault();
+   
+})
