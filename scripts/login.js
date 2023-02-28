ddd1b8f2ba72aa90b4aa30ef44158ecdf9081c86
Luis Alfonso Martinez Martinez <luisalfonsomartinezmartinez@outlook.es>
2023-02-28 01:16:13 -0600

    js basico in login

--- scripts/login.js
+++ scripts/login.js
@@ -7,23 +7,33 @@ class user{
 
 let formLogin =document.getElementById("form-login");
 
+var passRegex = new RegExp("^((?=.*[A-Z])(?=.*[0-9])(?=.*[-¡!¿?:;@#_$%^&,.{}=+*[])(?=.{8,}))");
+
 formLogin.addEventListener("submit", (e)=>{
+
     e.preventDefault();
-    let inputEmail = document.getElementById("email");
-    let inputContraseña =document.getElementById("password");
-
-    if(validatePassword(inputContraseña) && validateMail(inputEmail)){
-
-    }
-
+    let inputEmail = document.getElementById("email").value;
+    let inputContraseña =document.getElementById("password").value;
+    
+   // console.log(inputEmail);
+   // console.log(inputContraseña);
+   // console.log(validatePassword(inputContraseña));
+   
+    if(validatePassword(inputContraseña)){
+       alert("Contraseña y correo validos");
+       formLogin.reset();
+   }
+   else{
+       alert("Contraseña invalida, favor de volverla a ingresar ");
+       formLogin.reset();
+   }
 })
 
 function validatePassword(password){
-    
+    if(passRegex.test(password)){
+        return true;
+    }
+    else{
+        return false;
+    }
 }
-
-function validateMail(user){
-
-}
-
-//(¡”#$%&/=’?¡¿:;,.-_+*{][})
