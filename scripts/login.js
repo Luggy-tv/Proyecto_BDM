dfce9a950f3ad7dd4912fee42e94f5dcc825e274
Luis Alfonso Martinez Martinez <luisalfonsomartinezmartinez@outlook.es>
2023-02-27 23:50:55 -0600

    js

--- scripts/login.js
+++ scripts/login.js
@@ -8,6 +8,22 @@ class user{
 let formLogin =document.getElementById("form-login");
 
 formLogin.addEventListener("submit", (e)=>{
-   e.preventDefault();
-   //let input
+    e.preventDefault();
+    let inputEmail = document.getElementById("email");
+    let inputContraseña =document.getElementById("password");
+
+    if(validatePassword(inputContraseña) && validateMail(inputEmail)){
+
+    }
+
 })
+
+function validatePassword(password){
+    
+}
+
+function validateMail(user){
+
+}
+
+//(¡”#$%&/=’?¡¿:;,.-_+*{][})
